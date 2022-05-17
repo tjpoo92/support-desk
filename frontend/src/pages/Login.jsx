374f38cb5514faa35eaca6c7a74fb4f3e296c7e7
Tyler Pooschke <tyler.pooschke@pm.me>
2022-05-17 16:27:28 -0500

    section 18

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,98 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { login, reset } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
 function Login() {
-	return <div>Login</div>;
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const { email, password } = formData;
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const { user, isLoading, isSuccess, isError, message } = useSelector(
+		(state) => state.auth
+	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+		if (isSuccess || user) {
+			navigate("/");
+		}
+		dispatch(reset());
+	}, [isError, isSuccess, user, message, navigate, dispatch]);
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const userData = {
+			email,
+			password,
+		};
+		dispatch(login(userData));
+	};
+
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	return (
+		<>
+			<section className="heading">
+				<h1>
+					<FaSignInAlt /> Login
+				</h1>
+				<p>Please log in to get support</p>
+			</section>
+			<section className="form">
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<input
+							type="email"
+							id="email"
+							name="email"
+							className="form-control"
+							value={email}
+							onChange={onChange}
+							placeholder="Enter your email"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							id="password"
+							name="password"
+							className="form-control"
+							value={password}
+							onChange={onChange}
+							placeholder="Enter password"
+							required
+						/>
+					</div>
+
+					<div className="form-group">
+						<button className="btn btn-block">Submit</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,125 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
+
 function Register() {
-	return <div>Register</div>;
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		password: "",
+		password2: "",
+	});
+
+	const { name, email, password, password2 } = formData;
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+		if (isSuccess || user) {
+			navigate("/");
+		}
+		dispatch(reset());
+	}, [isError, isSuccess, user, message, navigate, dispatch]);
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		if (password !== password2) {
+			toast.error("Passwords do not match");
+		} else {
+			const userData = { name, email, password };
+			dispatch(register(userData));
+		}
+	};
+
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	return (
+		<>
+			<section className="heading">
+				<h1>
+					<FaUser /> Register
+				</h1>
+				<p>Please create an account</p>
+			</section>
+			<section className="form">
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<input
+							type="text"
+							id="name"
+							name="name"
+							className="form-control"
+							value={name}
+							onChange={onChange}
+							placeholder="Enter your name"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="email"
+							id="email"
+							name="email"
+							className="form-control"
+							value={email}
+							onChange={onChange}
+							placeholder="Enter your email"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							id="password"
+							name="password"
+							className="form-control"
+							value={password}
+							onChange={onChange}
+							placeholder="Enter password"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							id="password2"
+							name="password2"
+							className="form-control"
+							value={password2}
+							onChange={onChange}
+							placeholder="Confirm password"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<button className="btn btn-block">Submit</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
 
 export default Register;
